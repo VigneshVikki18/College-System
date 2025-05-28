8c8bd895dc073da875175b92a811477ba0a733ce
VigneshVikki18 <vikydgl2000@gmail.com>
2025-05-28 15:41:57 +0530

    Upto

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,11 @@ if (!fs.existsSync(uploadsDir)) {
 }
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true 
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 

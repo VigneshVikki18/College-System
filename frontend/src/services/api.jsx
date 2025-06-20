36e5e7c416ddf1864e83dcdd3b50b909294fb004
VigneshVikki18 <vikydgl2000@gmail.com>
2025-06-20 16:15:13 +0530

    Update

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: 'https://college-system-rc7x.onrender.com',
+  baseURL: 'https://college-system-rc7x.onrender.com/api',
 });
 
 // ✅ Add request interceptor to include token
